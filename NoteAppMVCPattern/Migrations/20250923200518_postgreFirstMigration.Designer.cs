d2cb7c4bb5e5628a1d627ae05dc1e6281d87dc8b
Emre <emre_erol17@hotmail.com>
2025-10-10 00:31:06 +0300

    postgresMigration folder is added

--- NoteAppMVCPattern/Migrations/20250923200518_postgreFirstMigration.Designer.cs
+++ NoteAppMVCPattern/Migrations/20250923200518_postgreFirstMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NoteAppMVCPattern.Models;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace NoteAppMVCPattern.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250923200518_postgreFirstMigration")]
+    partial class postgreFirstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
